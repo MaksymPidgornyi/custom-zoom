2cea3bcec2d0a5e7872e2181c71483ef8c292729
Maksym Pidhornyi <Maksym.pidgornyi@ukr.net>
2020-11-29 22:59:38 +0200

    Add chat handling and disconnect logic

--- public/script.js
+++ public/script.js
@@ -1,11 +1,15 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-  path: '/peerjs',
   host: '/',
-  port: '443'
+  port: '443',
+  path: '/peerjs'
 })
 let myVideoStream;
+let videoStreamArray;
+let isSharing;
+let webcamsRemovedBySharing = [];
+let myName = 'user';
 const myVideo = document.createElement('video')
 myVideo.muted = true;
 const peers = {}
@@ -36,13 +40,25 @@ navigator.mediaDevices.getUserMedia({
     }
   });
   socket.on("createMessage", message => {
-    $("ul").append(`<li class="message"><b>user</b><br/>${message}</li>`);
+    $("ul").append(`<li class="message"><b>${myName}</b><br/>${message}</li>`);
     scrollToBottom()
   })
+  
+})
+
+socket.on('screen-share', stream => {
+  console.log('Somebody is sharing the screen');
+})
+
+socket.on('stop-screen-share', stream => {
+  console.log('Somebody has stopped sharing');
 })
 
 socket.on('user-disconnected', userId => {
-  if (peers[userId]) peers[userId].close()
+  console.log(userId);
+  if (peers[userId]) {
+    peers[userId].close();
+  }
 })
 
 myPeer.on('open', id => {
@@ -101,20 +117,89 @@ const playStop = () => {
   }
 }
 
-const handleChat = () => {
-  document.querySelector('.main__right').display = none;
+const shareScreen = () => {
+  console.log("Sharing the screen");
+  let shareBtn = document.querySelector('.fa-angle-up');
 
+  if(shareBtn) {
+    startSharing();
+  } else {
+    stopSharing();
+  }
+}
+
+const startSharing = () => {
+  isSharing = true;
+    let webcams = videoGrid.getElementsByTagName('video')
+
+    for(let i = 0; i < webcams.length; i++) {
+      webcamsRemovedBySharing.push(webcams[i].srcObject);  
+    }
+
+    videoGrid.innerHTML = '';
+
+    navigator.mediaDevices.getDisplayMedia({cursor: true}).then(stream => {
+      video = document.createElement('video');
+      video.classList.add('sharing');
+      addVideoStream(video, stream);
+      console.log('emitting..')
+      socket.emit('share', stream);
+    })
+    setStopSharing();
+}
+
+const stopSharing = () => {
+  isSharing = false;
+    let sharingBlockObject = videoGrid.getElementsByTagName('video')[0].srcObject;
+    sharingBlockObject.getTracks()[0].stop();
+    videoGrid.innerHTML = '';
+    webcamsRemovedBySharing.forEach(videoStream => {
+      let video = document.createElement('video');
+      addVideoStream(video, videoStream);
+      socket.emit('stop-share', videoStream);
+    });
+    webcamsRemovedBySharing = [];
+    setStartSharing();
+}
+
+const handleChat = () => {
+  console.log('Hiding the chat panel')
+  let chatDisplay = document.querySelector('.main__right').style.display;
+
+  if(chatDisplay === '' || chatDisplay === 'flex') {
+    document.querySelector('.main__right').style.display = 'none';
+  } else {
+    document.querySelector('.main__right').style.display = 'flex';
+  }
 }
 
 const leaveMeeting = () => {
   socket.disconnect();
-  // socket.emit('close');
+  socket.emit('user-disconnected');
   setEmptyPage();
+}
+
+const setStopSharing = () => {
+  const html = `
+  <i class="fas fa-ban"></i>
+  <span>Stop sharing</span>
+  `
+
+  document.querySelector('.main__share_button').innerHTML = html;
+}
+
+const setStartSharing = () => {
+  const html = `
+  <i class="fas fa-angle-up"></i>
+  <span>Share</span>
+  `
+
+  document.querySelector('.main__share_button').innerHTML = html;
 }
 
 const setEmptyPage = () => {
   const html = `
-    <div>You left the meeting!</div>
+    <div class="leave-text">You left the meeting!</div>
   `
 
   document.querySelector('.main').innerHTML = html;
