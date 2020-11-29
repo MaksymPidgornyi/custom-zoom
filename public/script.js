1f989eafae8061a6b4018c649a4529dbe13d6d4f
Maksym Pidhornyi <Maksym.pidgornyi@ukr.net>
2020-11-29 22:59:38 +0200

    Add chat handling and disconnect logic

--- public/script.js
+++ public/script.js
@@ -6,6 +6,8 @@ const myPeer = new Peer(undefined, {
   port: '443'
 })
 let myVideoStream;
+let videoStreamArray;
+let isSharing;
 const myVideo = document.createElement('video')
 myVideo.muted = true;
 const peers = {}
@@ -42,7 +44,10 @@ navigator.mediaDevices.getUserMedia({
 })
 
 socket.on('user-disconnected', userId => {
-  if (peers[userId]) peers[userId].close()
+  console.log(userId);
+  if (peers[userId]) {
+    peers[userId].close();
+  }
 })
 
 myPeer.on('open', id => {
@@ -101,20 +106,72 @@ const playStop = () => {
   }
 }
 
-const handleChat = () => {
-  document.querySelector('.main__right').display = none;
+//TODO:fix it
+const shareScreen = () => {
+  console.log("Sharing the screen");
+  let shareBtn = document.querySelector('.fa-angle-up');
 
+  if(shareBtn) {
+
+    const displayMediaOptions = {
+      video: {
+        cursor: "always"
+      },
+
+      audio: false
+    }
+
+    videoStreamArray = myVideoStream.getVideoTracks();
+    isSharing = true;
+    
+    navigator.mediaDevices.getDisplayMedia(displayMediaOptions).then(stream => videoGrid.append(stream))
+      .catch(err => { console.error("Error:" + err); return null; });
+    setStopSharing();
+  } else {
+    isSharing = false;
+    // videoGrid = videoStreamArray;
+    setStartSharing();
+  }
+}
+
+const handleChat = () => {
+  console.log('Hiding the chat panel')
+  let chatDisplay = document.querySelector('.main__right').style.display;
+
+  if(chatDisplay === '' || chatDisplay === 'flex') {
+    document.querySelector('.main__right').style.display = 'none';
+  } else {
+    document.querySelector('.main__right').style.display = 'flex';
+  }
 }
 
 const leaveMeeting = () => {
   socket.disconnect();
-  // socket.emit('close');
+  socket.emit('user-disconnected');
   setEmptyPage();
+}
+
+const setStopSharing = () => {
+  const html = `
+  <i class="fas fa-ban"></i>
+  <span>Stop sharing</span>
+  `
+
+  document.querySelector('.main__share_button').innerHTML = html;
+}
+
+const setStartSharing = () => {
+  const html = `
+  <i class="fas fa-angle-up"></i>
+  <span>Share</span>
+  `
+
+  document.querySelector('.main__share_button').innerHTML = html;
 }
 
 const setEmptyPage = () => {
   const html = `
-    <div>You left the meeting!</div>
+    <div class="leave-text">You left the meeting!</div>
   `
 
   document.querySelector('.main').innerHTML = html;
